d7648e052288c6baf92f0ccd5b29067d88ab8568
Otavio <estudosprogotavio@gmail.com>
2025-04-17 18:01:25 -0300

    commit inicial

---
+++ src/main/java/dev/Zerpyhis/VibeEvents/VibeEventsApplication.java
@@ -0,0 +1,13 @@
+package dev.Zerpyhis.VibeEvents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VibeEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VibeEventsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/Zerpyhis/VibeEvents/VibeEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.Zerpyhis.VibeEvents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VibeEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
