3f21480d0aa3fce1e08d9fdf0be37a087db5feff
Otavio <estudosprogotavio@gmail.com>
2025-04-17 18:23:10 -0300

    feat:controller da entidade PersonEntity

---
+++ src/main/java/dev/Zerpyhis/VibeEvents/controller/PersonController.java
@@ -0,0 +1,50 @@
+package dev.Zerpyhis.VibeEvents.controller;
+
+import dev.Zerpyhis.VibeEvents.entitys.category.CategoryEntity;
+import dev.Zerpyhis.VibeEvents.entitys.person.PersonEntity;
+import dev.Zerpyhis.VibeEvents.records.DataCategory;
+import dev.Zerpyhis.VibeEvents.records.DataPerson;
+import dev.Zerpyhis.VibeEvents.service.CategoryService;
+import dev.Zerpyhis.VibeEvents.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/pessoas")
+public class PersonController {
+    @Autowired
+    private PersonService personService;
+
+    @PostMapping
+    public ResponseEntity<PersonEntity> cadastrar(@RequestBody DataPerson data) {
+        var pessoa = personService.registerPerson(data);
+        return ResponseEntity.ok(pessoa);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PersonEntity> atualizar(@PathVariable("id") Long id, @RequestBody DataPerson data) {
+        var pessoaAtualizada = personService.atualizationPerson(id, data);
+        return ResponseEntity.ok(pessoaAtualizada);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable("id") Long id) {
+        personService.deletePerson(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PersonEntity>> listarTodas() {
+        var pessoas = personService.ListAll();
+        return ResponseEntity.ok(pessoas);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PersonEntity> buscarPorId(@PathVariable("id") Long id) {
+        var pessoa = personService.listByid(id);
+        return ResponseEntity.ok(pessoa);
+    }
+}
