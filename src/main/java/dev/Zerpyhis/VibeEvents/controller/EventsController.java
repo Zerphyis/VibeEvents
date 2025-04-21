117b114105d710da33221cb204c7969af1655353
Otavio <estudosprogotavio@gmail.com>
2025-04-21 08:22:30 -0300

    feat: controller da classe EventsEntity

---
+++ src/main/java/dev/Zerpyhis/VibeEvents/controller/EventsController.java
@@ -0,0 +1,48 @@
+package dev.Zerpyhis.VibeEvents.controller;
+
+import dev.Zerpyhis.VibeEvents.entitys.events.EventsEntity;
+import dev.Zerpyhis.VibeEvents.records.DataEventsReponse;
+import dev.Zerpyhis.VibeEvents.records.RegisterEvent;
+import dev.Zerpyhis.VibeEvents.service.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/eventos")
+public class EventsController {
+    @Autowired
+    private EventService eventsService;
+
+    @PostMapping
+    public ResponseEntity<DataEventsReponse> createEvent(@RequestBody RegisterEvent data) {
+        DataEventsReponse newEvent = eventsService.createEvent(data);
+        return ResponseEntity.ok(newEvent);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<DataEventsReponse>> getAllEvents() {
+        List<DataEventsReponse> events = eventsService.findAllEvents();
+        return ResponseEntity.ok(events);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DataEventsReponse> getEventById(@PathVariable("id") Long id) {
+        DataEventsReponse event = eventsService.findEventById(id);
+        return ResponseEntity.ok(event);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<DataEventsReponse> updateEvent(@PathVariable("id") Long id, @RequestBody RegisterEvent data) {
+        DataEventsReponse updatedEvent = eventsService.updateEvent(id, data);
+        return ResponseEntity.ok(updatedEvent);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteEvent(@PathVariable("id") Long id) {
+        eventsService.deleteEvent(id);
+        return ResponseEntity.noContent().build();
+    }
+}
