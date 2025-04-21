65e17f2524cd1f84dd4a4303389ff4d88abe2bcf
Otavio <estudosprogotavio@gmail.com>
2025-04-21 08:22:46 -0300

    feat: exception personalizada da classe EventsEntity

---
+++ src/main/java/dev/Zerpyhis/VibeEvents/exceptions/EventsNotFoundException.java
@@ -0,0 +1,7 @@
+package dev.Zerpyhis.VibeEvents.exceptions;
+
+public class EventsNotFoundException extends RuntimeException {
+    public EventsNotFoundException(String message) {
+        super(message);
+    }
+}
