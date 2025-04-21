7ad142f6306a3d2e16809a576ce951a412d6f410
Otavio <estudosprogotavio@gmail.com>
2025-04-21 08:25:36 -0300

    refactor:adicionando @Repository no repositorio de EventsRepository

--- src/main/java/dev/Zerpyhis/VibeEvents/repositorys/EventsRepository.java
+++ src/main/java/dev/Zerpyhis/VibeEvents/repositorys/EventsRepository.java
@@ -2,6 +2,8 @@ package dev.Zerpyhis.VibeEvents.repositorys;
 
 import dev.Zerpyhis.VibeEvents.entitys.events.EventsEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EventsRepository extends JpaRepository<EventsEntity,Long> {
 }
