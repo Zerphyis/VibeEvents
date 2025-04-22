858ae25709b45e1a22d0d6fb8bf0d45202f11890
Otavio <estudosprogotavio@gmail.com>
2025-04-22 09:40:34 -0300

    feat: adicionando atributo quantityTicket em RegisterEvent

--- src/main/java/dev/Zerpyhis/VibeEvents/records/RegisterEvent.java
+++ src/main/java/dev/Zerpyhis/VibeEvents/records/RegisterEvent.java
@@ -2,11 +2,14 @@ package dev.Zerpyhis.VibeEvents.records;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import org.aspectj.weaver.ast.Not;
 
 import java.time.LocalDate;
 
 public record RegisterEvent(@NotBlank String name,
                             @NotBlank String location,
                             @NotNull LocalDate date,
-                            @NotNull Long categoryId) {
+                            @NotNull Long categoryId,
+                            @NotNull Integer quantityTicket
+) {
 }
