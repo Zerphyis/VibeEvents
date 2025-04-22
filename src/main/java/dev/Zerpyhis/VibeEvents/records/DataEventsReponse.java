d10787148588ce8a3d0cdc7518c1eed50c9bbf0c
Otavio <estudosprogotavio@gmail.com>
2025-04-22 09:50:21 -0300

    feat: adicionando atributo description em DataEventsResponse

--- src/main/java/dev/Zerpyhis/VibeEvents/records/DataEventsReponse.java
+++ src/main/java/dev/Zerpyhis/VibeEvents/records/DataEventsReponse.java
@@ -3,9 +3,12 @@ package dev.Zerpyhis.VibeEvents.records;
 
 import java.time.LocalDate;
 
-public record DataEventsReponse(   String name,
+public record DataEventsReponse(
+        String name,
+        String description,
         String location,
         LocalDate date,
-        Integer quantityTicket, Double priceTicket,
+        Integer quantityTicket,
+        Double priceTicket,
         String categoryName) {
 }
