46e016eb902c1c350efb9ae45b6e27f0a59fd3f8
Otavio <estudosprogotavio@gmail.com>
2025-04-23 14:54:14 -0300

    feat:adicionando GETTER E SETTER em TicketEntity

--- src/main/java/dev/Zerpyhis/VibeEvents/entitys/Ticket/TicketEntity.java
+++ src/main/java/dev/Zerpyhis/VibeEvents/entitys/Ticket/TicketEntity.java
@@ -80,6 +80,14 @@ public class TicketEntity {
         this.pricePaid = pricePaid;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public StatusTicket getStatus() {
         return status;
     }
