7bd6b7ebb8e48ba81264c94cb0ba5f183a9bed8c
Otavio <estudosprogotavio@gmail.com>
2025-04-23 13:42:49 -0300

    feat:record de construtor da entidade TicketEntity

---
+++ src/main/java/dev/Zerpyhis/VibeEvents/records/DataTicket.java
@@ -0,0 +1,18 @@
+package dev.Zerpyhis.VibeEvents.records;
+
+import dev.Zerpyhis.VibeEvents.entitys.Ticket.StatusTicket;
+import dev.Zerpyhis.VibeEvents.entitys.Ticket.TypeTicket;
+import dev.Zerpyhis.VibeEvents.entitys.events.EventsEntity;
+import dev.Zerpyhis.VibeEvents.entitys.person.PersonEntity;
+
+import java.time.LocalDate;
+
+public record DataTicket(
+        EventsEntity event,
+        PersonEntity person,
+        LocalDate dateBuy,
+        Double pricePaid,
+        StatusTicket status,
+        TypeTicket typeTicket
+) {
+}
