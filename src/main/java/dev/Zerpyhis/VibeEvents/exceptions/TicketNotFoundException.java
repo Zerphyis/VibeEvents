50201912838f8d3574478ec9f90153517ae78c75
Otavio <estudosprogotavio@gmail.com>
2025-04-23 13:42:13 -0300

    feat: excessão personalizada da entidade TicketEntity

---
+++ src/main/java/dev/Zerpyhis/VibeEvents/exceptions/TicketNotFoundException.java
@@ -0,0 +1,7 @@
+package dev.Zerpyhis.VibeEvents.exceptions;
+
+public class TicketNotFoundException extends RuntimeException {
+    public TicketNotFoundException(String message) {
+        super(message);
+    }
+}
