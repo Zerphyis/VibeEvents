39b1b313af4138a2d8156dd9c72a12e2f6812c1b
Otavio <estudosprogotavio@gmail.com>
2025-05-02 13:03:25 -0300

    refactor:adicionando  a GlobalException  o tratamento para exeception genericas

--- src/main/java/dev/Zerpyhis/VibeEvents/exceptions/GlobalException.java
+++ src/main/java/dev/Zerpyhis/VibeEvents/exceptions/GlobalException.java
@@ -3,6 +3,7 @@ package dev.Zerpyhis.VibeEvents.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -27,6 +28,21 @@ public class GlobalException {
         return buildResponse(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationErrors(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(err ->
+                errors.put(err.getField(), err.getDefaultMessage())
+        );
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, String>> handleGeneral(Exception ex) {
+        ex.printStackTrace();
+
+        return buildResponse("Ocorreu um erro interno. Tente novamente mais tarde.", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
     private ResponseEntity<Map<String, String>> buildResponse(String message, HttpStatus status) {
         Map<String, String> error = new HashMap<>();
